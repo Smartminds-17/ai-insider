9d9db84940cf6a9cf1588c0164b8ed612a60ef27
josephlyimo <josephlyimo201@gmail.com>
2026-09-03 16:32:43 +0300

    bug fix

--- components/PromptForm.tsx
+++ components/PromptForm.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import { useState } from "react";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 const EXAMPLES = ["Become a data analyst", "Learn UI design fundamentals", "Start making short-form video content"];
 
